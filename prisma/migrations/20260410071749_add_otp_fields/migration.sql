5f17d2dadba0f2e54bdd03d275f2ce0a63694a59
Nitin <nitinjha2609@gmail.com>
2026-04-17 12:09:20 +0530

    merge manish branch into main

--- prisma/migrations/20260410071749_add_otp_fields/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "isEmailVerified" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "otp" TEXT,
-ADD COLUMN     "otpExpiry" TIMESTAMP(3);
--- prisma/migrations/20260413061806_add_currency_fields/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Deal" ADD COLUMN     "currency" TEXT NOT NULL DEFAULT 'USD';
-
--- AlterTable
-ALTER TABLE "Lead" ADD COLUMN     "currency" TEXT NOT NULL DEFAULT 'USD';
-
--- AlterTable
-ALTER TABLE "Tenant" ADD COLUMN     "displayCurrency" TEXT NOT NULL DEFAULT 'USD';
